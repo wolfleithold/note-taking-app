d31abecd3349f86197a89023adc1e2c1c6df2ae5
Wolfgang Leithold <wolfleithold@gmail.com>
2024-07-20 22:00:44 -0700

    created server

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
